210f8bd2f66939b71d68f838052911ae6b05b993
Fredins <fredin.martin@gmail.com>
2022-03-15 00:38:04 +0100

    xmonad: add dwm zero action

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,7 +1,16 @@
+import           Control.Monad                  ( join )
 import qualified Data.Map                      as M
+import           Data.Maybe                     ( fromJust )
 import           Data.Monoid
 import           System.Exit
-import           XMonad
+import           XMonad                  hiding ( workspaces )
+import qualified XMonad
+import           XMonad.Actions.CopyWindow      ( copiesOfOn
+                                                , copy
+                                                , copyToAll
+                                                , killAllOtherCopies
+                                                , taggedWindows
+                                                )
 import           XMonad.Actions.CycleWS
 import           XMonad.Config.Desktop
 import           XMonad.Hooks.EwmhDesktops
@@ -12,7 +21,9 @@ import           XMonad.Hooks.ManageDocks       ( ToggleStruts(ToggleStruts)
 import           XMonad.Layout.LayoutModifier   ( ModifiedLayout )
 import           XMonad.Layout.NoBorders        ( smartBorders )
 import           XMonad.Layout.ResizableThreeColumns
-                                                ( ResizableThreeCol(..), MirrorResize(..) )
+                                                ( MirrorResize(..)
+                                                , ResizableThreeCol(..)
+                                                )
 import           XMonad.Layout.ResizableTile    ( MirrorResize(..)
                                                 , ResizableTall(..)
                                                 )
@@ -21,8 +32,13 @@ import           XMonad.Layout.Spacing
 import           XMonad.Layout.ThreeColumns     ( ThreeCol(..) )
 import           XMonad.Layout.ToggleLayouts
 import qualified XMonad.StackSet               as W
+import           XMonad.StackSet                ( integrate'
+                                                , peek
+                                                , stack
+                                                , workspaces
+                                                )
 import           XMonad.Util.Cursor
-import           XMonad.Util.Run                ( spawnPipe )
+
 
 main = do
   xmonad $ ewmhFullscreen . ewmh . docks $ desktopConfig
@@ -39,8 +55,8 @@ main = do
 myLayout = (smartBorders . avoidStruts) layouts
  where
   layouts  = tiled ||| threeCol ||| Full ||| Mirror tiled ||| simplestFloat
-  tiled    = ResizableTall 1 (1 / 20) (103/200) []
-  threeCol = ThreeCol 1 (1 / 20) (103/200) 
+  tiled    = ResizableTall 1 (1 / 20) (103 / 200) []
+  threeCol = ThreeCol 1 (1 / 20) (103 / 200)
 
 
 myTerminal = "alacritty"
@@ -50,7 +66,6 @@ myClickJustFocuses :: Bool
 myClickJustFocuses = False
 myBorderWidth = 2
 myModMask = mod4Mask
-myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 myNormalBorderColor = "#2E3440"
 myFocusedBorderColor = "#5E81AC"
 myStartupHook = setDefaultCursor xC_left_ptr
@@ -58,6 +73,7 @@ myStartupHook = setDefaultCursor xC_left_ptr
 myKeys conf@XConfig { XMonad.modMask = modm } =
   M.fromList
     $  [ ((modm, xK_n)                  , spawn $ XMonad.terminal conf)
+       , ((modm, xK_o)                  , withWindowSet dwmZero)
        , ((modm, xK_p)                  , spawn "dmenu_run")
        , ((modm, xK_c)                  , spawn "clipmenu")
        , ((modm .|. shiftMask, xK_c)    , kill)
@@ -91,6 +107,19 @@ myKeys conf@XConfig { XMonad.modMask = modm } =
        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0 ..]
        , (f  , m ) <- [(W.view, 0), (W.shift, shiftMask)]
        ]
+
+dwmZero :: WindowSet -> X ()
+dwmZero w = toggle $ filter (== fromJust (peek w)) ws
+ where
+  toggle xs | length xs >  1 = killAllOtherCopies
+            | otherwise      = windows copyToAll
+
+  ws = concatMap (integrate' . stack) $ workspaces w
+
+
+
+
+
 
 
 
