959cec55470479d3bcdb19afa9ee18ada4b749f9
Fredins <fredin.martin@gmail.com>
2022-03-14 23:01:30 +0100

    xmonad: add floating layout and resizable Tall

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,14 +5,20 @@ import           XMonad
 import           XMonad.Actions.CycleWS
 import           XMonad.Config.Desktop
 import           XMonad.Hooks.EwmhDesktops
-import           XMonad.Hooks.ManageDocks       ( avoidStruts
+import           XMonad.Hooks.ManageDocks       ( ToggleStruts(ToggleStruts)
+                                                , avoidStruts
                                                 , docks
-                                                , ToggleStruts(ToggleStruts)
                                                 )
 import           XMonad.Layout.LayoutModifier   ( ModifiedLayout )
 import           XMonad.Layout.NoBorders        ( smartBorders )
+import           XMonad.Layout.ResizableThreeColumns
+                                                ( ResizableThreeCol(..), MirrorResize(..) )
+import           XMonad.Layout.ResizableTile    ( MirrorResize(..)
+                                                , ResizableTall(..)
+                                                )
+import           XMonad.Layout.SimplestFloat    ( simplestFloat )
 import           XMonad.Layout.Spacing
-import           XMonad.Layout.ThreeColumns     ( ThreeCol(ThreeColMid) )
+import           XMonad.Layout.ThreeColumns     ( ThreeCol(..) )
 import           XMonad.Layout.ToggleLayouts
 import qualified XMonad.StackSet               as W
 import           XMonad.Util.Cursor
@@ -30,25 +36,11 @@ main = do
     }
 
 
-
-
 myLayout = (smartBorders . avoidStruts) layouts
  where
-  layouts = tiled ||| Mirror tiled ||| Full ||| threeCol 
-  threeCol = ThreeColMid 1 (3 / 100) (1 / 2)
-  tiled = Tall 1 0.05 0.515
-{-
-myLayout =
-  fullscreenFull
-    $   D.avoidStruts
-    $   smartBorders
-    $   toggleLayouts Full
-    $   tiled
-    ||| Mirror tiled
- where
-  tiled   = Tall 1 0.05 0.515
--}
-
+  layouts  = tiled ||| threeCol ||| Full ||| Mirror tiled ||| simplestFloat
+  tiled    = ResizableTall 1 (1 / 20) (103/200) []
+  threeCol = ThreeCol 1 (1 / 20) (103/200) 
 
 
 myTerminal = "alacritty"
@@ -80,6 +72,8 @@ myKeys conf@XConfig { XMonad.modMask = modm } =
        , ((modm .|. shiftMask, xK_k)    , windows W.swapUp)
        , ((modm, xK_h)                  , sendMessage Shrink)
        , ((modm, xK_l)                  , sendMessage Expand)
+       , ((modm .|. shiftMask, xK_h)    , sendMessage MirrorExpand)
+       , ((modm .|. shiftMask, xK_l)    , sendMessage MirrorShrink)
        , ((modm, xK_t)                  , withFocused $ windows . W.sink)
        , ((modm, xK_d)                  , sendMessage (IncMasterN 1))
        , ((modm, xK_i)                  , sendMessage (IncMasterN (-1)))
@@ -88,9 +82,6 @@ myKeys conf@XConfig { XMonad.modMask = modm } =
        , ((modm, xK_q)                  , spawn "xmonad --restart")
        , ((modm, xK_Tab)                , toggleWS)
        , ((modm .|. shiftMask, xK_q)    , spawn "lxqt-leave")
-       , ( (modm .|. shiftMask, xK_h)
-         , spawn ("echo \"" ++ help ++ "\" | xmessage -file -")
-         )
        ]
     ++ [ ((m .|. modm, k), windows $ f i)
        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
@@ -103,54 +94,3 @@ myKeys conf@XConfig { XMonad.modMask = modm } =
 
 
 
-
-help :: String
-help = unlines
-  [ "The default modifier key is 'Super'. Default keybindings:"
-  , ""
-  , "-- launching and killing programs"
-  , "mod-Shift-h      Launch this help window"
-  , "mod-n            Launch terminal"
-  , "mod-p            Launch dmenu"
-  , "mod-c            Launch clipmenu"
-  , "mod-Shift-c      Close/kill the focused window"
-  , "mod-Space        Rotate through the available layout algorithms"
-  , "mod-Shift-Space  Reset the layouts on the current workSpace to default"
-  , "mod-Shift-n      Resize/refresh viewed windows to the correct size"
-  , ""
-  , "-- move focus up or down the window stack"
-  , "mod-j          Move focus to the next window"
-  , "mod-k          Move focus to the previous window"
-  , "mod-Backspace  Move focus to the master window"
-  , ""
-  , "-- modifying the window order"
-  , "mod-Return   Swap the focused window and the master window"
-  , "mod-Shift-j  Swap the focused window with the next window"
-  , "mod-Shift-k  Swap the focused window with the previous window"
-  , ""
-  , "-- resizing the master/slave ratio"
-  , "mod-h  Shrink the master area"
-  , "mod-l  Expand the master area"
-  , ""
-  , "-- floating layer support"
-  , "mod-t  Push window back into tiling; unfloat and re-tile it"
-  , ""
-  , "-- increase or decrease number of windows in the master area"
-  , "mod-d   Increment the number of windows in the master area"
-  , "mod-d   Deincrement the number of windows in the master area"
-  , ""
-  , "-- quit, or restart"
-  , "mod-Shift-q  Quit xmonad"
-  , "mod-q        Restart xmonad"
-  , "mod-[1..9]   Switch to workSpace N"
-  , ""
-  , "-- Workspaces & screens"
-  , "mod-Shift-[1..9]   Move client to workspace N"
-  , "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3"
-  , "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3"
-  , ""
-  , "-- Mouse bindings: default actions bound to mouse events"
-  , "mod-button1  Set the window to floating mode and move by dragging"
-  , "mod-button2  Raise the window to the top of the stack"
-  , "mod-button3  Set the window to floating mode and resize by dragging"
-  ]
